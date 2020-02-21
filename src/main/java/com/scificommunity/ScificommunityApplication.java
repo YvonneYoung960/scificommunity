31c0239645f9ac9e4e5c5e2b657704cdbba9c419
Yvonne Young <1798556794@qq.com>
2020-02-21 11:44:49 +0800

    init repo

---
+++ src/main/java/com/scificommunity/ScificommunityApplication.java
@@ -0,0 +1,13 @@
+package com.scificommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScificommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScificommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scificommunity/ScificommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scificommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScificommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
